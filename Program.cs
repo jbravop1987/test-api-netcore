8a3c10d7dfe6f5a936aab79784f9c5d1e3ce0265
jbravop1987 <jbravop1987@gmail.com>
2025-12-03 16:37:52 -0500

    ajustes

--- Program.cs
+++ Program.cs
@@ -109,11 +109,11 @@ app.UseForwardedHeaders();
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
